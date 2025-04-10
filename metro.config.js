3d44a862d713b913bdfdd267704b4884537c2b36
Kwaku28 <rekenburg777@gmail.com>
2025-04-10 10:06:49 +0000

    Install and configure nativewind

---
+++ metro.config.js
@@ -0,0 +1,12 @@
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require('nativewind/metro');
+
+const config = getDefaultConfig(__dirname)
+
+// const {
+//   wrapWithReanimatedMetroConfig,
+// } = require('react-native-reanimated/metro-config');
+
+module.exports = withNativeWind(config, { input: './app/global.css' });
+
+// module.exports = wrapWithReanimatedMetroConfig(config);
