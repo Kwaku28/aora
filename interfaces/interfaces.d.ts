95a44d165e670c6c81e8f770be70d976ba47f31b
Kwaku28 <rekenburg777@gmail.com>
2025-04-11 23:22:22 +0000

    Add user model interface

---
+++ interfaces/interfaces.d.ts
@@ -0,0 +1,13 @@
+interface CreateUserParams {
+  email: string;
+  password: string;
+  username: string;
+}
+
+interface User {
+  accountId: string;
+  email: string;
+  username: string;
+  avatar: string;
+}
+
