307b979d67c64706d80d867a3f730a52566ca9a1
Kwaku28 <rekenburg777@gmail.com>
2025-04-15 10:52:48 +0000

    Create a custom hook to handle all api requests

---
+++ services/useAppwrite.ts
@@ -0,0 +1,29 @@
+import { Alert } from "react-native";
+import { useEffect, useState } from "react";
+
+const useAppwrite = <T,>(fn: () => Promise<T>) => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const res = await fn();
+      setData(res);
+    } catch (error) {
+      Alert.alert("Error", error instanceof Error ? error.message : "An unknown error occurred");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => fetchData();
+
+  return { data, loading, refetch };
+};
+
+export default useAppwrite;
